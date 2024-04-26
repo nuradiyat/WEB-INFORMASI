3c1ad1365c249ab9f3d3c6f79cfdb34c1439118e
nuradiyat <mnuradiyat@gmail.com>
2024-04-27 01:30:34 +0700

    merubah html css dan menambahkan video

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
 // membuat 3 variabe dengan type const
-const toggleMenu = document.getElementById("menu")
-const navbar = document.getElementById("navbar-nav")
+const toggleMenu = document.getElementById("menu");
+const navbar = document.getElementById("navbar-nav");
+let video = document.getElementById("video-banner");
+
+window.addEventListener("resize", function() {
+  resizeVideo();
+});
+
+function resizeVideo() {
+  let videoRatio = video.videoWidth / video.videoHeight;
+  let windowRatio = window.innerWidth / window.innerHeight;
+
+  if (windowRatio < videoRatio) {
+    // window is narrower
+    video.style.width = "100%";
+    video.style.height = "auto";
+  } else {
+    // video is narrower
+    video.style.width = "auto";
+    video.style.height = "100%";
+  }
+}
+
+resizeVideo(); // call the function once on page load
+
 
 // memanggil navbar
 toggleMenu.addEventListener("click", () => {
